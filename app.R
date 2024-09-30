dfb988e62cd6a1db6db8e0e78b25a1cc31c2ade2
B.K. Masinde <43029260+BMasinde@users.noreply.github.com>
2024-09-30 14:55:45 +0200

    first attempt rendering tif file
    First attempt at rendering tif file. FAILED

--- app.R
+++ app.R
@@ -12,6 +12,10 @@ library(leaflet)
 library(sf)
 library(RColorBrewer)
 
+# libraries for tif files
+library(leafem)
+library(stars)
+
 r_colors <- rgb(t(col2rgb(colors()) / 255))
 names(r_colors) <- colors()
 
@@ -34,6 +38,17 @@ path_mwakaboko2 <- "/Users/masinde/surfdrive/510/housing vulnerability (Echo IV)
 
 hse_svy <- read.csv(path_mwakaboko2, header = T, sep = ";", dec = ",")
 
+# tif file
+path_uav_mwakaboko <- "/Users/masinde/surfdrive/510/xter-HV/uav/mwakaboko1_ortho_corrected_big.tif"
+tif = system.file(path_uav_mwakaboko, package = "stars")
+x1 <- read_stars(tif)
+x1 <-  x1[, , , 3] # what the hell does it mean band 3?
+
+tmpfl = tempfile(fileext = ".tif")
+
+write_stars(st_warp(x1, crs = 4326), tmpfl)
+
+
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
   
@@ -44,10 +59,19 @@ server <- function(input, output, session) {
 
   output$mymap <- renderLeaflet({
     leaflet() %>%
-      addProviderTiles(providers$CartoDB.Positron,
-                       options = providerTileOptions(noWrap = TRUE)
-      ) %>%
-      addMarkers(data = coords())
+      # addProviderTiles(providers$CartoDB.Positron,
+      #                options = providerTileOptions(noWrap = TRUE)
+      # ) %>%
+      addTiles() %>%
+      addGeotiff(
+        file = tmpfl,
+        opacity = 0.9,
+        colorOptions =  colorOptions(
+          palette = hcl.colors(256, palette = "inferno")
+          , na.color = "transparent"
+        )
+      ) #%>%
+      #addMarkers(data = coords())
   })
 }
 
