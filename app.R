777bc364379b708b3568e435f112f9162d46232b
B.K. Masinde <43029260+BMasinde@users.noreply.github.com>
2024-11-08 14:43:23 +0100

    display street view images
    Interactive displaying of street view images

--- app.R
+++ app.R
@@ -11,92 +11,135 @@ library(shiny)
 library(leaflet)
 library(sf)
 library(RColorBrewer)
-
-# libraries for tif files
 library(leafem)
-#library(stars)
-#library (raster)
 library(terra)
 
 
-# Define UI for application that draws a histogram
+# Define UI for application
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("'Do No Harm' Valorisation Workshop"),
+  titlePanel("'Do No Harm' Valorisation Workshop"),
+  
+  fluidRow(
+    # Map output on the left
+    column(width = 6,
+           leafletOutput("mymap", width = "100%", height = "800px")
+    ),
     
-    # leaflet 
-    leafletOutput("mymap", width = "50%", height = "800px"),
-    p(),
-    #actionButton("recalc", "New points")
-    
+    # Image display on the right
+    column(width = 6,
+           uiOutput("image_display", height = "400px")  # Display the selected image
+    )
+  )
 )
 
-# household survey data
-path_mwakaboko2 <- "/Users/masinde/surfdrive/510/housing vulnerability (Echo IV)/TA Mwakaboko GVH Mwakaboko2.csv"
+# path list for building materials
+path_list_bm_points <- list("data/mwakaboko_bm_clipped_6.geojson") 
 
-hse_svy <- read.csv(path_mwakaboko2, header = T, sep = ";", dec = ",")
+#hse_svy <- read.csv(path_mwakaboko2, header = T, sep = ";", dec = ",")
 
-# tif file
-path_uav_mwakaboko <- "data/mwakaboko1_clipped2.tif"
+# List of input file paths for UAV images
+path_list <- list("data/mwakaboko1_clipped2.tif", "data/mwakaboko1_clipped3.tif",
+                  "data/mwakaboko1_clipped4.tif", 
+                  "data/mwakaboko1_clipped5.tif",
+                  "data/mwakaboko1_clipped6.tif")
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
   
-  # Load the GeoTIFF file using terra (handles multi-band rasters)
-  uav_tif <- rast(path_uav_mwakaboko)
+  # List to store output rasters and extents
+  raster_list <- list()
+  extent_list <- list()
   
-  # Ensure that all bands have the same extent by comparing each band's extent to the first band's extent
-  extents_match <- all(sapply(2:nlyr(uav_tif), function(i) ext(uav_tif[[i]]) == ext(uav_tif[[1]])))
-  
-  if (!extents_match) {
-    stop("The bands do not have the same extent. You may need to resample or align them.")
-  }
-  
-  # If the raster has 4 bands, use bands 1-3 for RGB and optionally band 4 for NIR or other purposes
-  # This assumes bands 1-3 are RGB and band 4 is NIR
-  if (nlyr(uav_tif) >= 4) {
-    uav_rgbnir <- uav_tif[[1:4]]  # Extracting all 4 bands
-  } else {
-    stop("The TIFF file does not have 4 bands.")
-  }
-  
-  # Check if the CRS is EPSG:4326 (WGS84) and reproject if necessary
+  # Define target CRS
   target_crs <- "EPSG:4326"  # WGS84 projection
   
-  # Compare the CRS of the raster with the target CRS
-  if (crs(uav_rgbnir) != target_crs) {
-    uav_rgbnir <- terra::project(uav_rgbnir, target_crs)
+  # Load GeoJSON points from path_list_bm_points
+  bm_points <- st_read(path_list_bm_points[[1]])
+  
+  # Loop over each file path
+  for (i in seq_along(path_list)) {
+    
+    # Load the GeoTIFF file
+    uav_tif <- rast(path_list[[i]])
+    
+    # Ensure that all bands have the same extent
+    extents_match <- all(sapply(2:nlyr(uav_tif), function(j) ext(uav_tif[[j]]) == ext(uav_tif[[1]])))
+    
+    if (!extents_match) {
+      stop("The bands do not have the same extent. You may need to resample or align them.")
+    }
+    
+    # Check if the raster has at least 4 bands and select bands 1-4
+    if (nlyr(uav_tif) >= 4) {
+      uav_rgbnir <- uav_tif[[1:4]]  # Extracting all 4 bands
+    } else {
+      stop("The TIFF file does not have 4 bands.")
+    }
+    
+    # Reproject to target CRS if necessary
+    if (crs(uav_rgbnir) != target_crs) {
+      uav_rgbnir <- terra::project(uav_rgbnir, target_crs)
+    }
+    
+    # Resample to ensure all bands have the same resolution
+    uav_rgbnir <- resample(uav_rgbnir, uav_rgbnir[[1]], method = "bilinear")
+    
+    # Convert terra::SpatRaster to raster::Raster and store in list
+    raster_list[[i]] <- raster::stack(uav_rgbnir)
+    
+    # Get the extent of the raster and store in list
+    extent_list[[i]] <- ext(uav_rgbnir)
   }
   
-  # Ensure all bands have the same resolution (just in case)
-  uav_rgbnir <- resample(uav_rgbnir, uav_rgbnir[[1]], method = "bilinear")
-  
-  # Convert terra::SpatRaster to raster::Raster
-  uav_rgbnir_raster <- raster::stack(uav_rgbnir)
-  
-  # Get the extent of the raster (bounding box)
-  uav_extent <- ext(uav_rgbnir)  # terra::ext() gives xmin, xmax, ymin, ymax
+  # At this point, `raster_list` contains your processed rasters, and `extent_list` contains the extents.
+  # For example, you can access the first raster with `raster_list[[1]]` and its extent with `extent_list[[1]]`.
   
   # Reactive coordinates for household survey points
-  coords <- eventReactive(input$recalc, {
-    cbind(as.numeric(hse_svy$gps_longitude), as.numeric(hse_svy$gps_latitude))
-  }, ignoreNULL = FALSE)
+  #coords <- eventReactive(input$recalc, {
+  #  cbind(as.numeric(hse_svy$gps_longitude), as.numeric(hse_svy$gps_latitude))
+  #}, ignoreNULL = FALSE)
   
-
+  # Reactive variable to store the selected image path
+  selected_image <- reactiveVal()
+  
   output$mymap <- renderLeaflet({
     leaflet() %>%
-     addProviderTiles(providers$CartoDB.Positron,
-                      options = providerTileOptions(noWrap = TRUE)
-       ) %>%
-      #addTiles() #%>%
-      # Add the raster image to the leaflet map using addRasterImage
-      addRasterRGB(uav_rgbnir_raster, r = 1, g = 2, b = 3, opacity = 0.8) #%>% # RGB + NIR (optional alpha) #%>%  # RGB + NIR, with alpha channel as NIR (optional)
-      #fitBounds(lng1 = uav_extent$xmin, lat1 = uav_extent$ymin, 
-      #          lng2 = uav_extent$xmax, lat2 = uav_extent$ymax)
-      # Optionally add markers for the survey data
-      #addMarkers(data = coords(), popup = ~paste("Household ID:", hse_svy$household_id))
-      # addMarkers(data = coords())
+      addProviderTiles(providers$CartoDB.Positron,
+                       options = providerTileOptions(noWrap = TRUE)
+      ) %>%
+      
+      # Loop through raster_list to add each raster image
+      {
+        map <- .
+        for (raster in raster_list) {
+          map <- map %>% addRasterRGB(raster, r = 1, g = 2, b = 3, opacity = 0.8)
+        }
+        map
+      } %>%
+    
+      # Add the GeoJSON points layer
+      addCircleMarkers(data = bm_points, 
+                       color = "blue", 
+                       radius = 5, 
+                       fillOpacity = 0.7,
+                       layerId = ~image_key,  # Use imageKey as layer ID for easy retrieval
+                       popup = ~paste("ID:", image_key))  # Show imageKey in popup
+  })
+  
+  # Observe map clicks to get the clicked point’s imageKey
+  observeEvent(input$mymap_marker_click, {
+    click <- input$mymap_marker_click
+    if (!is.null(click$id)) {  # Check if there’s a valid imageKey
+      # Construct the path to the image using the imageKey
+      image_path <- paste0("images/", click$id, ".png")
+      selected_image(image_path)  # Update reactive value with image path
+    }
+  })
+  
+  # Display the selected image
+  output$image_display <- renderUI({
+    req(selected_image())  # Wait until selected_image has a value
+    tags$img(src = selected_image(), width = "100%", height = "auto")
   })
 }
 
